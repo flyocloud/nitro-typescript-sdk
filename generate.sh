12ec7409abc804cab028422afd133b8a2281bee9
Nadar <git@nadar.io>
2023-11-08 11:00:00 +0000

    use config file

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 openapi-generator-cli generate -i  https://api.flyo.cloud/nitro/v1/openapi \
     -g typescript-fetch \
     -o . \
-    --additional-properties=npmName=@flyo/nitro-typescript,npmVersion=1.0.0
+    -c config.json
