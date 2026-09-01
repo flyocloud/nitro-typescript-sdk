11e489d432d9a297922152baa9de3ea280a4212f
Basil Suter <git@nadar.io>
2026-09-01 06:34:29 +0000

    remove old code

--- apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ConfigApi';
-export * from './EntitiesApi';
-export * from './PagesApi';
-export * from './SearchApi';
-export * from './SitemapApi';
-export * from './VersionApi';
